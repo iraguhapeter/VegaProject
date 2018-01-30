04ac36c8a4f027729250056a4577d8d87f32e0e1
Pierre Iraguha <petrus@Pierres-MacBook-Pro.local>
2018-01-30 12:09:24 -0500

    Photo constraints shifted in configuration file

--- Controllers/PhotosController.cs
+++ Controllers/PhotosController.cs
@@ -6,6 +6,7 @@ using AutoMapper;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using Vega.Core;
 using Vega.Core.Models;
 
@@ -14,49 +15,54 @@ namespace Vega.Controllers
     [Route("/api/vehicles/{vehicleId}/photos")]
     public class PhotosController : Controller
     {
-    private readonly int MAX_BYTES = 1 * 1024 * 1024;
-    private readonly string[] ACCEPTED_FILE_TYPES = new[] { ".jpg", ".jpeg", ".png" };
-    private readonly IHostingEnvironment host;
-    private readonly IVehicleRepository repository;
-    private readonly IUnitOfWork unitOfWork;
-    private readonly IMapper mapper;
-    public PhotosController(IHostingEnvironment host, IVehicleRepository repository, IUnitOfWork unitOfWork, IMapper mapper)
-    {
-      this.mapper = mapper;
-      this.unitOfWork = unitOfWork;
-      this.repository = repository;
-      this.host = host;
-    }
-    [HttpPost]
-    public async Task<IActionResult> Upload(int vehicleId, IFormFile file)
-    {
-      var vehicle = await repository.GetVehicle(vehicleId, includeRelated: false);
-      if (vehicle == null)
-        return NotFound();
-      
-      if (file == null) return BadRequest("Null file");
-      if (file.Length == 0) return BadRequest("Empty file");
-      if (file.Length > MAX_BYTES) return BadRequest("Max file size exceeded");
-      if (!ACCEPTED_FILE_TYPES.Any(s => s == Path.GetExtension(file.FileName))) return BadRequest("Invalid file type.");
+      private readonly IHostingEnvironment host;
+      private readonly IVehicleRepository repository;
+      private readonly IUnitOfWork unitOfWork;
+      private readonly IMapper mapper;
+      private readonly PhotoSettings photoSettings;
 
-      var uploadsFolderPath = Path.Combine(host.WebRootPath, "uploads");
-      if (!Directory.Exists(uploadsFolderPath))
-        Directory.CreateDirectory(uploadsFolderPath);
-
-      var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
-      var filePath = Path.Combine(uploadsFolderPath, fileName);
-
-      using (var stream = new FileStream(filePath, FileMode.Create))
+        public PhotosController(IHostingEnvironment host, 
+                                IVehicleRepository repository, 
+                                IUnitOfWork unitOfWork, 
+                                IMapper mapper,
+                                IOptionsSnapshot<PhotoSettings> options)
+        {
+          this.mapper = mapper;
+          this.photoSettings = options.Value;
+          this.unitOfWork = unitOfWork;
+          this.repository = repository;
+          this.host = host;
+        }
+      [HttpPost]
+      public async Task<IActionResult> Upload(int vehicleId, IFormFile file)
       {
-        await file.CopyToAsync(stream);
-      }
-
-      var photo = new Photo { FileName = fileName };
-      vehicle.Photos.Add(photo);
-      await unitOfWork.CompleteAsync();
-
-      return Ok(mapper.Map<Photo, PhotoResource>(photo));
-    }
+        var vehicle = await repository.GetVehicle(vehicleId, includeRelated: false);
+        if (vehicle == null)
+          return NotFound();
         
+        if (file == null) return BadRequest("Null file");
+        if (file.Length == 0) return BadRequest("Empty file");
+        if(file.Length > photoSettings.MaxBytes) return BadRequest("Max file exceeded");
+
+
+
+        var uploadsFolderPath = Path.Combine(host.WebRootPath, "uploads");
+        if (!Directory.Exists(uploadsFolderPath))
+          Directory.CreateDirectory(uploadsFolderPath);
+
+        var fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+        var filePath = Path.Combine(uploadsFolderPath, fileName);
+
+        using (var stream = new FileStream(filePath, FileMode.Create))
+        {
+          await file.CopyToAsync(stream);
+        }
+
+        var photo = new Photo { FileName = fileName };
+        vehicle.Photos.Add(photo);
+        await unitOfWork.CompleteAsync();
+
+        return Ok(mapper.Map<Photo, PhotoResource>(photo));
+      }   
     }
 }
--- Startup.cs
+++ Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using vega.Persistence;
 using Vega.Core;
+using Vega.Core.Models;
 using Vega.Persistence;
 
 namespace Vega
@@ -23,6 +24,8 @@ namespace Vega
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.Configure<PhotoSettings>(Configuration.GetSection("PhotoSettings"));
+            
             services.AddScoped<IVehicleRepository, VehicleRepository>();
             services.AddScoped<IUnitOfWork, UnitOfWork>();
 
